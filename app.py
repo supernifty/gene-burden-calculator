b80168003728564b3920cea8242495f8f3ea1934
Peter Georgeson <peter@supernifty.org>
2016-09-11 17:02:17 +1000

    db integration

--- app.py
+++ app.py
@@ -4,22 +4,43 @@
 '''
 
 import flask
+import sqlite3
+
 app = flask.Flask(__name__)
 
+DB = "./exac.db"
+
+### database access
+def db():
+    db_instance = getattr(flask.g, '_database', None)
+    if db_instance is None:
+        db_instance = flask.g._database = sqlite3.connect(DB)
+    return db_instance
+
+def query_db(query, args=(), one=False):
+    cur = db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db_instance = getattr(flask.g, '_database', None)
+    if db_instance is not None:
+        db_instance.close()
+
+### business logic
 def process():
-    print("1")
+    '''
+        analysis
+    '''
     filter_type = flask.request.form['filter_type']
-    print("2")
     if filter_type not in ('cadd', 'condel', 'sift', 'polyphen'):
         flask.render_template('main.html', error='Invalid filter type')
     try:
-        print("a")
         filter_value = float(flask.request.form['filter_value'])
-        print("b")
     except ValueError:
-        print("c")
         return flask.render_template('main.html', error='Invalid filter value')
-    print("3")
 
     burdens = flask.request.form['burdens'].split('\n')
     result = []
@@ -27,12 +48,20 @@ def process():
         fields = burden.strip().split(',')
         if len(fields) != 2:
             return flask.render_template('main.html', error='Incorrect burden format on line {}'.format(line))
-        result.append({'gene': fields[0], 'count': fields[1]})
 
-    return flask.render_template('results.html', result=result)
+        # find matching genes
+        matches = query_db("select count(*) from exac where gene = ? and {} >= ?".format(filter_type), [fields[0], filter_value], one=True)[0]
+        
+        result.append({'gene': fields[0], 'burden': fields[1], 'matches': matches})
 
+    return flask.render_template('results.html', result=result, filter_type=filter_type, filter_value=filter_value)
+
+### front end
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    '''
+        main entry point
+    '''
     if flask.request.method == 'POST':
         return process()
     else:
